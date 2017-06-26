fc459b1a33d15e1c84016c4f3b84a07c50c9e467
Pascal Lamblin <lamblinp@iro.umontreal.ca>
2017-06-26 00:23:36 -0400

    VGG16 + k-NN example

---
+++ notebooks/vgg16.py
@@ -0,0 +1,101 @@
+# Original file:
+# https://github.com/Lasagne/Recipes/blob/master/modelzoo/vgg16.py
+
+# VGG-16, 16-layer model from the paper:
+# "Very Deep Convolutional Networks for Large-Scale Image Recognition"
+# Original source: https://gist.github.com/ksimonyan/211839e770f7b538e2d8
+# License: see http://www.robots.ox.ac.uk/~vgg/research/very_deep/
+
+# Download pretrained weights from:
+# https://s3.amazonaws.com/lasagne/recipes/pretrained/imagenet/vgg16.pkl
+
+import numpy as np
+import skimage.transform
+
+from lasagne.layers import InputLayer
+from lasagne.layers import Conv2DLayer as ConvLayer
+from lasagne.layers import DenseLayer
+from lasagne.layers import NonlinearityLayer
+from lasagne.layers import DropoutLayer
+from lasagne.layers import Pool2DLayer as PoolLayer
+from lasagne.nonlinearities import softmax
+from lasagne.utils import floatX
+
+
+def build_model():
+    net = {}
+    net['input'] = InputLayer((None, 3, 224, 224))
+    net['conv1_1'] = ConvLayer(
+        net['input'], 64, 3, pad=1, flip_filters=False)
+    net['conv1_2'] = ConvLayer(
+        net['conv1_1'], 64, 3, pad=1, flip_filters=False)
+    net['pool1'] = PoolLayer(net['conv1_2'], 2)
+    net['conv2_1'] = ConvLayer(
+        net['pool1'], 128, 3, pad=1, flip_filters=False)
+    net['conv2_2'] = ConvLayer(
+        net['conv2_1'], 128, 3, pad=1, flip_filters=False)
+    net['pool2'] = PoolLayer(net['conv2_2'], 2)
+    net['conv3_1'] = ConvLayer(
+        net['pool2'], 256, 3, pad=1, flip_filters=False)
+    net['conv3_2'] = ConvLayer(
+        net['conv3_1'], 256, 3, pad=1, flip_filters=False)
+    net['conv3_3'] = ConvLayer(
+        net['conv3_2'], 256, 3, pad=1, flip_filters=False)
+    net['pool3'] = PoolLayer(net['conv3_3'], 2)
+    net['conv4_1'] = ConvLayer(
+        net['pool3'], 512, 3, pad=1, flip_filters=False)
+    net['conv4_2'] = ConvLayer(
+        net['conv4_1'], 512, 3, pad=1, flip_filters=False)
+    net['conv4_3'] = ConvLayer(
+        net['conv4_2'], 512, 3, pad=1, flip_filters=False)
+    net['pool4'] = PoolLayer(net['conv4_3'], 2)
+    net['conv5_1'] = ConvLayer(
+        net['pool4'], 512, 3, pad=1, flip_filters=False)
+    net['conv5_2'] = ConvLayer(
+        net['conv5_1'], 512, 3, pad=1, flip_filters=False)
+    net['conv5_3'] = ConvLayer(
+        net['conv5_2'], 512, 3, pad=1, flip_filters=False)
+    net['pool5'] = PoolLayer(net['conv5_3'], 2)
+    net['fc6'] = DenseLayer(net['pool5'], num_units=4096)
+    net['fc6_dropout'] = DropoutLayer(net['fc6'], p=0.5)
+    net['fc7'] = DenseLayer(net['fc6_dropout'], num_units=4096)
+    net['fc7_dropout'] = DropoutLayer(net['fc7'], p=0.5)
+    net['fc8'] = DenseLayer(
+        net['fc7_dropout'], num_units=1000, nonlinearity=None)
+    net['prob'] = NonlinearityLayer(net['fc8'], softmax)
+
+    return net
+
+
+def prep_image(im, mean_image):
+    # Resize so smallest dim = 256, preserving aspect ratio
+    h, w, _ = im.shape
+
+    if h < w:
+        new_shape = (256, w*256//h)
+    else:
+        new_shape = (h*256//w, 256)
+    im = skimage.transform.resize(im, new_shape,
+                                  preserve_range=True,
+                                  mode='constant')
+
+    # Central crop to 224x224
+    h, w, _ = im.shape
+    im = im[h//2-112:h//2+112, w//2-112:w//2+112]
+    # If all values are bellow 1, we should keep it as float
+    # otherwise, we need to convert it to uint8 to display well
+    if im.max() <= 1:
+        rawim = im
+    else:
+        rawim = np.copy(im).astype('uint8')
+
+    # Shuffle axes to c01
+    im = np.swapaxes(np.swapaxes(im, 1, 2), 0, 1)
+
+    # Drop the alpha channel, VGG16 don't support it
+    im = im[:3]
+
+    # Convert to BGR
+    im = im[::-1, :, :]
+
+    return rawim, floatX(im[np.newaxis])
